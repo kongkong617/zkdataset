1895857772ba43a6aa12c7224c594d09d3775396
kongkong@swift-win <jyk104@163.com>
2018-06-14 14:31:30 +0800

    add data_column add partitioner

---
+++ src/python/zk/dataset/data_column.py
@@ -0,0 +1,135 @@
+"""
+DataColumns, a representation of table-like data.
+from https://github.com/Hong-Xiang/dxlearn/tree/master/src/python/dxl/learn/dataset
+"""
+import h5py
+import tables as tb
+import numpy as np
+from typing import Dict, Iterable
+
+
+class DataColumns:
+    def __init__(self, data):
+        self.data = self._process(data)
+        self._capacity_cache = None
+        self._category_cache = None
+        self._iterator = None
+
+    def _process(self, data):
+        return data
+
+    @property
+    def columns(self):
+        return self.data.keys()
+
+    def _calculate_capacity(self):
+        raise NotImplementedError
+
+    def _calculate_category(self):
+        raise NotImplementedError
+
+    @property
+    def capacity(self):
+        if self._capacity_cache is not None:
+            return self._capacity_cache
+        else:
+            return self._calculate_capacity()
+
+    @property
+    def category(self):
+        if self._category_cache is not None:
+            return self._category_cache
+        else:
+            return self._calculate_category()
+
+    @property
+    def shapes(self):
+        raise NotImplementedError
+
+    @property
+    def types(self):
+        raise NotImplementedError
+
+    def _make_iterator(self):
+        raise NotImplementedError
+
+    def __next__(self):
+        if self._iterator is None:
+            self._iterator = iter(self)
+        return next(self._iterator)
+
+    def __iter__(self):
+        return self._make_iterator()
+
+
+# self add 
+class UnbalancedNotFixedPyTablesColums(DataColumns):
+    """
+    Unbalanced:
+        Each class has different numbers of samples.
+            so need to be re-sampling to same
+    NotFixed:
+        Each sample has a different shape, 
+            then make a sample a dataset and let all datasets in a node.
+
+            The dataset name must be encode like:
+                L_1_agltS1qfOZiUvzBy24uc
+            where:
+                `L` is a meaningless letter, just for escape namewarrning
+                `_` is a split mark
+                `1` is an sample class label
+                `agltS1qfOZiUvzBy24uc` is an unique sample name
+
+            The dataset colnames must be encode like:
+                x = .... , represent data
+                y = .... , represent label
+    """
+    class KEYS:
+        MARK = "_"
+        COLNAME_X = 'x'
+        COLNAME_Y = 'y'
+
+    def __init__(self, path_file, path_dataset, partitioner):
+        super().__init__((path_file, path_dataset))
+        self._partitioner = partitioner
+
+    def _process(self, data):
+        path_file, path_dataset = data
+        self._file = tb.open_file(str(path_file))
+        self._node = self._file.get_node(path_dataset)
+
+        self._category_cache = {}
+        self._dataset_nodes = self._node._f_list_node()
+
+        for i, dataset in enumerate(self._dataset_nodes):
+            label = dataset.name.split(self.KEYS.MARK)[1]
+            index = self._category_cache.get(label)
+            if index is None:
+                index = []
+            
+            index.append(i)
+            self._category_cache.update({label : index})
+
+    def _calculate_capacity(self):
+        _capacity = {}
+        for k, v in self._category_cache.items():
+            _capacity.update({k : len(v)})
+
+        self._capacity_cache = _capacity
+        return _capacity
+
+    def __getitem__(self, i):
+        node = self._dataset_nodes[i]
+        x = self.KEYS.COLNAME_X
+        y = self.KEYS.COLNAME_Y
+        return node[x], node[y]
+        
+    def _make_iterator(self):
+        return self._partitioner.partition(self)
+
+    @property
+    def columns(self):
+        return [self.KEYS.COLNAME_X, self.KEYS.COLNAME_Y]
+
+    def close(self):
+        self._file.close()
---
+++ src/python/zk/dataset/partitioner.py
@@ -0,0 +1,95 @@
+"""
+Dataset partition utilities.
+Class Partition is a index provider, thus provide index of next sample in ndarray.
+from https://github.com/Hong-Xiang/dxlearn/tree/master/src/python/dxl/learn/dataset
+"""
+from collections import UserDict
+from typing import Dict, Iterable
+from .resampling import get_resampling
+import numpy as np
+
+
+
+class Partitioner:
+    def _get_original_indices(self, data_column):
+        if isinstance(data_column.capacity, int):
+            return range(data_column.capacity)
+        elif isinstance(data_column.capacity, Dict):
+            return data_column.category
+
+    def _get_valid_indices(self, indicies):
+        return indicies
+
+    def partition(self, data_column: Iterable) -> Iterable:
+        def valid_index_generator(data_column, indices):
+            for i in indices:
+                yield data_column[i]
+
+        return valid_index_generator(
+            data_column,
+            self._get_valid_indices(self._get_original_indices(data_column)))
+
+    def get_capacity(self, data_columns):
+        raise NotImplementedError
+
+
+# self add
+class CrossValidatePartitioner(Partitioner):
+    def __init__(self, nb_blocks, in_blocks, resampling=None):
+        super().__init__()
+        self._nb_blocks = nb_blocks
+        if isinstance(in_blocks, int):
+            in_blocks = [in_blocks]
+        self._in_blocks = in_blocks
+        self._resampling = resampling
+
+    def _get_valid_indices(self, indices):
+        result = []
+        # indices is a category Dict
+        if isinstance(indices, Dict):
+            class_info = {}
+            for k, v in indices:
+                class_info.update({
+                    k: self.make_valid_indices(v)
+                })
+            # resamping
+            if self._resampling:
+                result = get_resampling(class_info, self._resampling)
+            else:
+                for i in class_info.values():
+                    result.extend(i)
+                # shuffle
+                np.random.shuffle(result)
+        # indices is range like 
+        else:
+            result = self.make_valid_indices(indices)
+
+        return tuple(result)
+
+    def make_valid_indices(self, indices):
+        result = []
+        len_block = len(indices) // self._nb_blocks
+        for b in self._in_blocks:
+            result += [
+                indices[i] for i in range(b * len_block, (b + 1) * len_block)
+            ]
+        return result
+
+    def get_capacity(self, data_columns):
+        partition_capacity = []
+
+        if isinstance(data_columns.capacity, int):
+            len_block = data_columns.capacity // self._nb_blocks
+            partition_capacity =  len_block * len(self._in_blocks)
+        else:
+            for _, v in data_columns.category.items():
+                if self._resampling:
+                    partition_capacity += self._resampling
+                else:
+                    kc = (len(v) // self._nb_blocks) * len(self._in_blocks)
+                    partition_capacity += kc
+
+        return partition_capacity
+        
+
+
