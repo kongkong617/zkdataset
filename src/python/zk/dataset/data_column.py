5898fde99c2770d58e40bda6f207635297d0ce4f
kongkong@swift-win <jyk104@163.com>
2018-06-18 22:06:05 +0800

    fix data input

--- src/python/zk/dataset/data_column.py
+++ src/python/zk/dataset/data_column.py
@@ -120,9 +120,9 @@ class UnbalancedNotFixedPyTablesColums(DataColumns):
         return _capacity
 
     def __getitem__(self, i):
-        node = self._dataset_nodes[i]
-        x = node[0][self.KEYS.COLNAME_X]
-        y = node[0][self.KEYS.COLNAME_Y]
+        datarow = self._dataset_nodes[i][0]
+        x = datarow[self.KEYS.COLNAME_X]
+        y = datarow[self.KEYS.COLNAME_Y]
 
         y = np.array(y)
         # print(y.shape)
