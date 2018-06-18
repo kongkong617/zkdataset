5cb9bd056c256e19afc910dd1cf765476b6c9a46
kongkong@swift-win <jyk104@163.com>
2018-06-18 21:25:17 +0800

    debug data

--- src/python/zk/dataset/data_column.py
+++ src/python/zk/dataset/data_column.py
@@ -125,13 +125,13 @@ class UnbalancedNotFixedPyTablesColums(DataColumns):
         y = node[0][self.KEYS.COLNAME_Y]
 
         y = np.array(y)
-        print(y.shape)
+        # print(y.shape)
 
-        # if len(x.shape) == 3:
-        #     shape = [-1] + [i for i in x.shape]
-        #     x = np.reshape(x, shape)
+        if len(x.shape) == 3:
+            shape = [-1] + [i for i in x.shape]
+            x = np.reshape(x, shape)
         x = np.array(x)
-        print(x.shape)
+        # print(x.shape)
 
         return (x, y)
 
